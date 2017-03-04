4a81c8ab7be01d4e34518ff9e2fc826e44553134
Nkuli <nkulix@fpm.co.za>
2017-03-04 14:37:38 +0200

    My Django Girls App, first commit

---
+++ Begin.py
@@ -0,0 +1,7 @@
+def calc_months_alive(age):
+    months_in_year = 12
+    months_alive = age * months_in_year
+    return months_alive
+my_age = calc_months_alive(2)
+print(my_age)
+print(calc_months_alive(27))
